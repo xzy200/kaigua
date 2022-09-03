97c087f0d3a2d9f59fd89eb875fdfbea4b6c39ab
yaoniplan <yaoniplan@gmail.com>
2022-09-03 08:23:29 +0800

    挑战：圆的面积

---
+++ challenge/CircleArea1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import math
+r = 2
+CircleArea = math.pi * r ** 2
+print("CircleArea = ", '{:.10f}'.format(CircleArea))
---
+++ challenge/CircleArea2.py
@@ -0,0 +1,3 @@
+import math # 导入数学库
+area = math.pi * 2 * 2 # 半径为2的圆面积公式
+print("{:.10f}".format(area)) # 格式化输出圆面积，并保留10位小数
